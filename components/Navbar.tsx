344035647efc0092cd62056660733271439f2f9b
Pablo <pab.michelini@gmail.com>
2025-08-06 18:37:33 +0200

    include login

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
 import NavItems from './NavItems'
 
 const NavBar = () => {
@@ -12,7 +13,14 @@ const NavBar = () => {
         </Link>
         <div className='flex items-center gap-8'>
            <NavItems/>
-           <p>Sign in</p>
+           <SignedOut>
+                <SignInButton>
+                  <button className='btn-signin'>Sign In</button>
+                </SignInButton>
+           </SignedOut>
+           <SignedIn>
+                <UserButton/>
+           </SignedIn>
         </div>
     </nav>
 
