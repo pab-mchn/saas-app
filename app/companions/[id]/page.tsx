550cf2816a2fd556938897e6aea3d91707c8b8cc
Pablo <pab.michelini@gmail.com>
2025-08-13 12:01:20 +0200

    CompanionComponent handle call functions

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getSubjectColor } from "@/lib/utils";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import Image from "next/image";
+import CompanionComponent from "@/components/CompanionComponent";
 
 interface CompanionSessionPageProps {
   params: Promise<{id: string}>
@@ -10,11 +11,12 @@ interface CompanionSessionPageProps {
 
 const CompanionSessions = async ({params}: CompanionSessionPageProps) => {
   const {id} = await params;
-  const {name, subject, title, topic, duration} = await getCompanion(id)
+  const companion = await getCompanion(id)
   const user = await currentUser()
 
-  if(!user) redirect('./sign-in')
+  const {name, subject, title, topic, duration} = companion
 
+  if(!user) redirect('./sign-in')
   if(!name) redirect('./companions')
 
   return (
@@ -42,6 +44,13 @@ const CompanionSessions = async ({params}: CompanionSessionPageProps) => {
             {duration} minutes
           </div>
         </article>
+        <CompanionComponent
+        {...companion}
+        companionId={id}
+        userName={user.firstName!}
+        userImage={user.imageUrl!}
+
+        />
     </main>
   )
 }
